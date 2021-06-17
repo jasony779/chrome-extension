055dcc4b11e4cc536b5fed62605ac586e697792d
Jason Yoon <jasony779@gmail.com>
2021-06-17 12:09:01 -0700

    picture working

--- main.js
+++ main.js
@@ -1,23 +1,78 @@
+let timeLeft = 20;
+let timeActive = false;
 
-document.addEventListener('DOMContentLoaded', ()=>{
-const timeLeftDisplay = document.querySelector ('#time-left')
-const startBtn = document.querySelector('#start-button')
-timeLeft = 20
+document.addEventListener('DOMContentLoaded', () => {
+	const timeLeftDisplay = document.querySelector('#time-left');
+	const startBtn = document.querySelector('#start-button');
 
-function countDown(){
-    setInterval(function(){
-        if(timeLeft<= 0){
-            clearInterval(timeLeft = 0)
-        }
-        timeLeftDisplay.innerHTML = timeLeft
-        timeLeft--;
-    },500) 
-
-}
+	
 
 
+	function countDown() {
+		setInterval(function () {
+		
+   
+			// if (timeLeft === -1) {
+			// 	console.log('thisis zero')
+				
+				// var div = document.getElementsByClassName('wrapper');
+				// // div.id = 'box';
+				// var img = document.createElement('IMG');
+				// img.src = 'random.png';
+				// img.id = 'theImage'
+				// div.appendChild(img);
+				// document.body.appendChild(div);
 
-startBtn.addEventListener ('click', countDown)
+				if (timeLeft===0){
+	
+					stopTimer();
+				
+					const div = document.getElementById('wrapper');
+				
+			
+					var img = document.createElement('IMG');
+					img.src = 'random.png';
+					img.id = 'theImage'
+					div.appendChild(img);
+					document.body.appendChild(div);
+				
+				
+
+				};
+				
+				// clearInterval((timeLeft = 0));
+			// }
+			
+			timeLeftDisplay.innerHTML = timeLeft;
+			timeLeft--; 
+
+			if (timeLeft===0){
+	
+				stopTimer();
+			
+				const div = document.getElementById('wrapper');
+				console.log(div);
+				div.innerHTML += 'hello world'
+			
+				// div.id = 'box';
+				// var img = document.createElement('IMG');
+				// img.src = 'random.png';
+				// img.id = 'theImage'
+				// div.appendChild(img);
+				// document.body.appendChild(div);
+			
+			
+			
+			};
+			
+
+			
+		}, 500);
 
 
-})
+
+	}
+const stopTimer=()=>clearInterval(countDown);
+
+	startBtn.addEventListener('click', countDown);
+});
